d9b5dad3019109e00f96de9e992f7dd85113b1db
Eugene <sirius.blet@gmail.com>
2023-06-18 13:49:51 +0200

    initial commit

---
+++ awesome-compose/react-java-mysql/backend/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+-- INSERT INTO example.test(name) values ('Docker M2 !  ');
---
+++ awesome-compose/react-java-mysql/backend/target/classes/schema.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE IF NOT EXISTS example.greetings (
+--     id INTEGER AUTO_INCREMENT,
+--     name varchar(50) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS example.test (
+--     id INTEGER AUTO_INCREMENT,
+--     name varchar(50) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
